6d6dc2a8433cd53973ba7e427afb1abdcf0c9379
Ying-Hsin Hua <yxh448@student.bham.ac.uk>
2025-07-20 16:36:32 +0100

    chore: update Tailwind CSS configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+   extend: {  // extend 裡加入自定義動畫
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        }
+      },
+      animation: {
+        "fade-in": "fadeIn 1s ease-in-out",
+      }
+    },
   },
   plugins: [],
 }
