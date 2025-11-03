0cdd7140c575935fedb8c184f8e9fcfd820c76ad
Ying-Hsin Hua <yxh448@student.bham.ac.uk>
2025-11-03 13:37:45 +0800

    chore: secure DB connection using .env

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from backend.base import Base
+from dotenv import load_dotenv
+import os
 
-engine = create_engine("postgresql://REDACTED@localhost:5432/tailor_taipei")
+load_dotenv()  
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 
 Base.metadata.create_all(engine)
 print("Tables created successfully.")
